6ec2b53e1a8c9cde8d39434eb89b0202ef485658
yprdaily <yuya.suzuki@prdaily.jp>
2026-07-27 10:18:24 +0900

    Delete extension/config.runtime.example.js

--- extension/config.runtime.example.js
+++
@@ -1,8 +0,0 @@
-export const CORPBOT_RUNTIME = {
-  UI_MODE: "dev",
-  DEV_UI_URL: "http://127.0.0.1:5173/",
-  PROD_UI_PATH: "widget/index.html",
-  OAUTH_CLIENT_ID: "234528761794-ho34gcfmrbo0dmrr6b6lnu8j7i30iedd.apps.googleusercontent.com",
-  OAUTH_SCOPES: ["openid", "email", "profile"],
-  OAUTH_HD_HINT: "prdaily.jp",
-};
